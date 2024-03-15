798b18d467f556c31990bf00fe31a5048ed1a13e
mnuel1 <mnuelmrin21@gmail.com>
2024-03-16 01:16:08 +0800

    update

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 # app.py
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import base64
 from main import main
 app = Flask(__name__)
 CORS(app, origins='http://localhost:5173')
@@ -11,13 +12,25 @@ def get_data():
 
 @app.route('/api/get/shortest/path/<start_node>/<end_node>')
 def get_shortest_path(start_node, end_node):
-    shortest_path, shortest_distance = main(start_node, end_node)  # Assuming main() takes start_node and end_node as arguments
-    if shortest_path:
-        print(f"Shortest distance from node {start_node} to node {end_node}: {shortest_distance}")
-        print("Shortest path:", shortest_path)
-    else:
-        print(f"There is no path from node {start_node} to node {end_node}")
+    shortest_path, shortest_distance, encoded_image_bytes  = main(start_node, end_node)  # Assuming main() takes start_node and end_node as arguments
+    # if shortest_path:
+    #     print(f"Shortest distance from node {start_node} to node {end_node}: {shortest_distance}")
+    #     print("Shortest path:", shortest_path)
+    # else:
+    #     print(f"There is no path from node {start_node} to node {end_node}")
+   # Encode the bytes to base64
+    encoded_image_base64 = encoded_image_bytes.decode('utf-8')
 
-    return {'data': {'shortest_path': shortest_path, 'shortest_distance': shortest_distance}}
+    # Construct the response JSON
+    response = {
+        'data': {
+            'shortest_path': shortest_path,
+            'shortest_distance': shortest_distance,
+            'encoded_image': encoded_image_base64
+        }
+    }
+
+    return jsonify(response)
+    # return {'data': {'shortest_path': shortest_path, 'shortest_distance': shortest_distance, 'encoded_image': encoded_image}}
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import open3d as o3d
+import base64
 import matplotlib.pyplot as plt
 import heapq
 
@@ -36,33 +37,41 @@ def is_corner(color, threshold=0.1, green=0.7, blue=0.8):
     return r < threshold and g > green and b > blue
 def dijkstra(graph, start, end):
     distances = {node: float('inf') for node in graph}
+    path_distances = [65.76, 46.5, 46.5, 65.76, 46.5, 46.5, 65.76, 46.5, 46.5, 46.5, 46.5, 46.5, 46.5]
     distances[start] = 0
     priority_queue = [(0, start)]
     previous_nodes = {}
+    edge_weights = {}  # Store edge weights in the shortest path
 
     while priority_queue:
         current_distance, current_node = heapq.heappop(priority_queue)
-
         if current_node == end:
             path = []
+            path_weights = []  # Store weights of edges in the path
             while current_node != start:
                 path.append(current_node)
-                current_node = previous_nodes[current_node]
+                parent_node = previous_nodes[current_node]
+                edge_weight = graph[parent_node][current_node]
+                path_weights.append(edge_weight)
+                current_node = parent_node
             path.append(start)
             path.reverse()
-            return distances[end], path
+            path_weights.reverse()
+            return distances[end], path, path_weights
 
         if current_distance > distances[current_node]:
             continue
 
         for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
+            distance = current_distance + path_distances[weight]
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
                 heapq.heappush(priority_queue, (distance, neighbor))
                 previous_nodes[neighbor] = current_node
+                edge_weights[(current_node, neighbor)] = weight
 
-    return float('inf'), None
+    return float('inf'), None, None
+
 
 def main(start_node, end_node):
     # image path
@@ -172,7 +181,7 @@ def main(start_node, end_node):
         connections = []
         graph = {letters[i]: [] for i in range(len(average_x))}    
         flag = 0
-        
+        index = 0
         for i in range(0, len(average_x)):
             connection = []  
 
@@ -183,7 +192,8 @@ def main(start_node, end_node):
                     connection.append([diagonal_y, j])            
                     for number in range(len(average_x)):                                
                         if average_x[number] == j and abs(diagonal_y - average_y[number]) <= 400:                                                                                
-                                dist = 65.76
+                                dist = index
+                                index += 1
                                 graph[letters[i]].append({letters[number]: dist})
                                 graph[letters[number]].append({letters[i]: dist})
                                 flag = 1       
@@ -192,8 +202,8 @@ def main(start_node, end_node):
                     diagonal_y += 1
                 if flag == 0 : 
                     connection = [] 
-            
-            connections.append(connection)            
+            if connection : 
+                connections.append(connection)            
             connection = []  
             flag = 0
 
@@ -205,7 +215,8 @@ def main(start_node, end_node):
                     for number in range(len(average_x)):   
                         if average_y[number] == j and abs(diagonal_y - average_x[number]) <= 400:
                             if letters[i] == 'D':                                                
-                                dist = 65.76
+                                dist = index
+                                index += 1
                                 graph[letters[i]].append({letters[number]: dist})
                                 graph[letters[number]].append({letters[i]: dist})
                                 flag = 1
@@ -216,7 +227,8 @@ def main(start_node, end_node):
             if flag == 0 : 
                 connection = [] 
             
-            connections.append(connection)
+            if connection : 
+                connections.append(connection) 
             connection = []  
             flag = 0
 
@@ -226,7 +238,8 @@ def main(start_node, end_node):
                 for number in range(len(average_x)):                                
                     if average_y[number] == j and abs(average_x[i] - average_x[number]) <= 10:                        
                         if letters[i] != 'C' and letters[number] != 'D':                                                
-                            dist = 46.5
+                            dist = index
+                            index += 1
                             graph[letters[i]].append({letters[number]: dist})
                             graph[letters[number]].append({letters[i]: dist})
                             flag = 1
@@ -237,7 +250,8 @@ def main(start_node, end_node):
             if flag == 0 : 
                 connection = [] 
             
-            connections.append(connection)            
+            if connection : 
+                connections.append(connection)             
             connection = []  
             flag = 0
 
@@ -247,7 +261,8 @@ def main(start_node, end_node):
                     connection.append([average_y[i], j])        
                     if average_x[number] == j and abs(average_y[i] - average_y[number]) <= 10:
                        
-                        dist = 46.5
+                        dist = index
+                        index += 1
                         graph[letters[i]].append({letters[number]: dist})
                         graph[letters[number]].append({letters[i]: dist})
                                         
@@ -257,13 +272,14 @@ def main(start_node, end_node):
             if flag == 0 : 
                 connection = [] 
                         
-            connections.append(connection)
+            if connection : 
+                connections.append(connection) 
             connection = [] 
             flag = 0       
         return connections, graph
 
     connections, graph = find_connections(average_x, average_y)
-
+   
     # separate it properly
     new_data = {}
     for key, value in graph.items():
@@ -272,22 +288,43 @@ def main(start_node, end_node):
             new_value.update(item)
         new_data[key] = new_value
 
-    conn_average_x = []
-    conn_average_y = []
-    for connected_groups in connections:  
-        for connected_group in connected_groups:                    
+    # print(new_data)
+    shortest_distance, shortest_path, distances = dijkstra(new_data, start_node, end_node)
+    # print(distances)
+    # if shortest_path:
+    #     print(f"Shortest distance from node {start_node} to node {end_node}: {shortest_distance}")
+    #     print("Shortest path:", shortest_path)
+        
+    # else:
+    #     print(f"There is no path from node {start_node} to node {end_node}")
+  
+    # Loop through indices specified in the distances list
+    for idx in distances:
+        print(idx)
+        connected_groups = connections[idx]  # Assuming connections is defined somewhere
+        for connected_group in connected_groups:
             elementImage[connected_group[1], connected_group[0]] = [255, 0, 0]  # Red color for vertices
-    
+   
+    # # Create a window and display the image in full screen mode
+    # cv2.namedWindow('Image with vertices highlighted', cv2.WINDOW_NORMAL)
+ 
+    # # Display the image with vertices highlighted
+    # cv2.imshow('Image with vertices highlighted', elementImage)
+    # cv2.waitKey(0)  # Wait for any key press
+    # cv2.destroyAllWindows()
+    # Get the encoded image
+            
+    # Your existing code to generate the encoded image...
+    _, buffer = cv2.imencode('.png', elementImage)
+    encoded_image_bytes = base64.b64encode(buffer)
+    return shortest_path, shortest_distance, encoded_image_bytes
+    # return None
+# main(start_node='A',end_node='B')
+    #  conn_average_x = []
+    # conn_average_y = []
+  
     # start_node = 'A'
     # end_node = 'B'
-    shortest_distance, shortest_path = dijkstra(new_data, start_node, end_node)
-    if shortest_path:
-        print(f"Shortest distance from node {start_node} to node {end_node}: {shortest_distance}")
-        print("Shortest path:", shortest_path)
-        return shortest_path, shortest_distance
-    else:
-        print(f"There is no path from node {start_node} to node {end_node}")
-    return None
     # for idx, group in enumerate(connections):
     #     x_values = [vertex[0] for vertex in group]
     #     y_values = [vertex[1] for vertex in group]
